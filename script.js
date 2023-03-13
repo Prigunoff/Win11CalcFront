0ec8f97bebb6ad79d75d749ac03bf6eb98d48c6f
Даниил Прыгунов <idanil430@gmail.com>
2023-03-13 15:50:08 +0200

    Updated buttons, scripts for buttons

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 function appendToResult(val) {
-	document.getElementById('result').value += val;
-	}
+  document.getElementById("result").value += val;
+}
 
 function clearResult() {
-	document.getElementById('result').value = '';
-	}
+  document.getElementById("result").value = "";
+}
 
 function calculate() {
-	try {
-	var result = eval(document.getElementById('result').value);
-	document.getElementById('result').value = result;
-		} catch (error) {
-			alert('Invalid input!');
-		}
-	}
+  try {
+    var result = eval(document.getElementById("result").value);
+    document.getElementById("result").value = result;
+  } catch (error) {
+    alert("Invalid input!");
+  }
+}
+
+function sqrt() {
+  let result = document.getElementById("result");
+  let value = result.value;
+  if (value) {
+    let sqrtValue = Math.sqrt(parseFloat(value));
+    if (isNaN(sqrtValue)) {
+      result.value = "Error";
+    } else {
+      result.value = sqrtValue;
+    }
+  }
+}
+
+function deleteLastDigit() {
+	var result = document.getElementById("result").value;
+	document.getElementById("result").value = result.substring(0, result.length-1);
+}
+
+function squareNumber() {
+	let input = document.getElementById("result");
+	let inputValue = input.value;
+	let square = parseFloat(inputValue) ** 2;
+	input.value = square;
+}
+
+function twoSqrtX() {
+	let x = document.getElementById("result").value;
+	let result = 2 * Math.sqrt(x);
+	document.getElementById("result").value = result;
+  }
