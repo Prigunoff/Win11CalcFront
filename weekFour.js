4c15bfd472baf8188f8eca2ac81a13950a94cb93
Даниил Прыгунов <idanil430@gmail.com>
2023-03-19 12:45:18 +0200

    A list task developed

--- weekFour.js
+++ weekFour.js
@@ -36,4 +36,52 @@ function reverseArrayInPlace(array) {
     }
 }
 
-console.log(reverseArray(["A", "B", "C"]));
+//A List
+function arrayToList(array) {
+    let list = null;
+    for (let i = array.length - 1; i >= 0; i--) {
+        list = { value: array[i], rest: list };
+    }
+    return list;
+}
+
+function listToArray(list) {
+    let array = [];
+    for (let node = list; node; node = node.rest) { // moving from one node to the next node in the linked list
+        array.push(node.value);
+    }
+    return array;
+}
+
+function prepend(element, list) {
+    return { value: element, rest: list };
+}
+
+
+function main(){
+    console.log(range(1, 10));
+    // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    console.log(range(5, 2, -1));
+    // → [5, 4, 3, 2]
+    console.log(sum(range(1, 10)));
+    // → 55
+ 
+
+    console.log(reverseArray(["A", "B", "C"]));
+    // → ["C", "B", "A"];
+    let arrayValue = [1, 2, 3, 4, 5];
+    reverseArrayInPlace(arrayValue);
+    console.log(arrayValue);
+    // → [5, 4, 3, 2, 1]
+
+    console.log(arrayToList([10, 20]));
+    // → {value: 10, rest: {value: 20, rest: null}}
+    console.log(listToArray(arrayToList([10, 20, 30])));
+    // → [10, 20, 30]
+    console.log(prepend(10, prepend(20, null)));
+    // → {value: 10, rest: {value: 20, rest: null}}
+    // console.log(nth(arrayToList([10, 20, 30]), 1)); // not done yet
+    // → 20
+}
+
+main();
